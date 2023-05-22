f1000cf17a26331bbb1511b57abb51e7db796fbd
Aman23716 <amanzadr@gmail.com>
2023-05-22 15:10:25 +0600

    dlya Maksata

--- src/main/java/org/OptimaBus/UI/pages/MainPage.java
+++ src/main/java/org/OptimaBus/UI/pages/MainPage.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 public class MainPage extends BasePage{
 
+
     @FindBy(xpath = "(.//span[@class='q-btn__content text-center col items-center q-anchor--skip justify-center row'])[3]")
     public WebElement calcBTN;
 
@@ -42,22 +43,51 @@ public class MainPage extends BasePage{
    @FindBy(xpath = ".//a[@class='user-toggle no-caret']")
    public WebElement chooseCompany;
 
-   @FindBy(xpath = ".//button[@class='aside-dropdown-item dropdown-item user-text text-dark'][3]")
+   @FindBy(xpath = ".//button[@class='aside-dropdown-item dropdown-item user-text text-dark'][2]")
    public WebElement chooseUtes;
 
+   @FindBy(xpath = "(.//span[@class='q-ml-auto'])[1]")
+   public WebElement KURSSOM;
+
+    @FindBy(xpath = ".//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--standard q-btn--rectangle bg-primary text-white q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase payment__btn lite-shadow']")
+    public WebElement CreateBTN;
+
+    @FindBy(xpath = ".//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable p-select-header-close q-pa-none']")
+    public WebElement ExitBTN;
 
 
 
+
+
+
+
+
+
+
+
+
+
+    public MainPage CreateBTN(){
+        elementActions.click(ExitBTN).click(CreateBTN);
+        return this;
+    }
+
+   public MainPage GetValueSomSum(){
+       KURSSOM.getText();
+       return this;
+   }
+
+   
+   
 
    public MainPage Znacheniya(){
        HUI.getText();
-
        return this;
    }
 
 
     public MainPage chooseKurs(){
-        elementActions.click(calcBTN).click(VidKursov).click(clickOnKPK).sendKeys(inputSum, "10000");
+        elementActions.click(calcBTN).click(VidKursov).click(clickOnKPK)/*.sendKeysInt(inputSum, mockDataGenerator.generateMockValue());*/.sendKeysInt(inputSum,10000);
 
         return this;
     }
