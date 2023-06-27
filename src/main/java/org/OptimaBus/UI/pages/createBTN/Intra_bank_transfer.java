846b49ed0a86aac2e8ab9ac8eaffb2e93ede9488
Aman23716 <amanzadr@gmail.com>
2023-06-27 14:18:10 +0600

    aa

--- src/main/java/org/OptimaBus/UI/pages/createBTN/Intra_bank_transfer.java
+++ src/main/java/org/OptimaBus/UI/pages/createBTN/Intra_bank_transfer.java
@@ -24,7 +24,40 @@ public class Intra_bank_transfer extends BasePage {
     public WebElement inputSummuPlateja;
     @FindBy(xpath = "(.//input[@class='q-field__native q-placeholder'])[4]")
     public WebElement inputKodPlateja;
+    @FindBy(xpath = "(.//div[@class='ob-select'])[15]")
+    public WebElement chooseNalog;
     @FindBy(xpath = ".//textarea[@class='q-field__native q-placeholder']")
     public WebElement inputNaznachenie;
+    @FindBy(xpath = ".//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--unelevated q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase sign-btn transfer-btn']")
+    public WebElement naPodpisBTN;
+
+
+
+
+
+
+
+    @FindBy(xpath = "(.//div[@role='alert'])[1]")
+    public WebElement zapolnitePoleError;
+    @FindBy(xpath = "(.//div[@role='alert'])[2]")
+    public WebElement schetPoluchatelyaError;
+    @FindBy(xpath = "(.//div[@role='alert'])[3]")
+    public WebElement documentNumberError;
+    @FindBy(xpath = "(.//div[@role='alert'])[4]")
+    public WebElement summaError;
+    @FindBy(xpath = "(.//div[@role='alert'])[5]")
+    public WebElement naznachenieError;
+
+
+
+    public void CheckNaPodpisBTN(){
+        elementActions.click(schetSpisaniyaBTN).click(vnutriBankBTN).sendKeys(inputSchetZa4isleniya,"1091808755280112").inputDocumentNumber1(inputDocumNumber)
+                .sendKeysInt(inputSummuPlateja,100).sendKeys(inputKodPlateja,"1").click(chooseNalog).inputNaznachenieField1(inputNaznachenie).click(naPodpisBTN);
+    }
+
+
+
+
+
 
 }
