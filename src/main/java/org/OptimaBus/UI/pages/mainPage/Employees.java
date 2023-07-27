8de4c8adcf9921346b516bbc2f3a43bc8f809f48
Zhyldyz <ubarikova.jyldyz@gmail.com>
2023-07-27 20:31:25 +0600

    add new tests

--- src/main/java/org/OptimaBus/UI/pages/mainPage/Employees.java
+++ src/main/java/org/OptimaBus/UI/pages/mainPage/Employees.java
@@ -5,16 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Employees extends BasePage {
-    @FindBy (xpath ="//a[@href='/employees'] ")
-     public WebElement employeesBtn;
+    @FindBy(xpath = "//a[@href='/employees'] ")
+    public WebElement employeesBtn;
+
+    @FindBy(xpath = "//h3[@class='q-ma-none header__title']")
+    public WebElement employeeText;
 
 
-
-
-
-
-
-    public Employees clickEmployeesBtn(){
+    public Employees clickEmployeesBtn() {
         elementActions.click(employeesBtn);
         return this;
     }
