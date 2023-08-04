03e34d00d663c3a9615a84f56f2b60a2c0e9fe50
Aman23716 <amanzadr@gmail.com>
2023-08-04 14:28:19 +0600

    Revert "Maks"

--- src/main/java/org/OptimaBus/UI/pages/mainPage/OperationHistory.java
+++ src/main/java/org/OptimaBus/UI/pages/mainPage/OperationHistory.java
@@ -15,16 +15,14 @@ public class OperationHistory extends BasePage {
     @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[1]")
     public WebElement allBTN;
     @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[2]")
-    public WebElement ojidaetOtpravki;
-    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[3]")
     public WebElement naRedactirov;
-    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[4]")
+    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[3]")
     public WebElement ispolnennye;
-    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[5]")
+    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[4]")
     public WebElement otklonennye;
-    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[6]")
+    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[5]")
     public WebElement otkazanoBankom;
-    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[7]")
+    @FindBy(xpath = "(.//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase filter-btn'])[6]")
     public WebElement moiChernoviki;
 
     /**FILTERFIELD**/
@@ -63,22 +61,68 @@ public class OperationHistory extends BasePage {
     public WebElement summaDO;
     @FindBy(xpath = ".//button[@class='q-btn q-btn-item non-selectable no-outline q-btn--standard q-btn--rectangle bg-primary text-white q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase payment btn lite-shadow']")
     public WebElement primenitBTN;
+    @FindBy(xpath = ".//input[@class='q-field__input q-placeholder col']")
+    public WebElement volutaField;
+    @FindBy(xpath = ".//div[@class='ob-select']")
+    public WebElement KGSChoose;
+
+
     /**ASSERTIONS**/
     @FindBy(xpath = ".//div[@class='ellipsis']")
     public WebElement assertions;
     @FindBy(xpath = "(.//div[@class='p1'])[1]")
     public WebElement assertions2;
+    @FindBy(xpath = "(.//div[@class='transaction__title p1 d-none'])[1]")
+    public WebElement companiya;
+    @FindBy(xpath = "(.//div[@class='transaction__amount text-weight-medium p1'])[1]")
+    public WebElement summaAssertion;
+    @FindBy(xpath = "(.//span[@class='text-disabled'])[1]")
+    public WebElement documentNumberAsseert;
+    @FindBy(xpath = ".//div[@class='transaction-group__title text-h5']")
+    public WebElement dataAssertion;
 
-
-
-
-
-
-
-
-    public void TipPlatejaFilter(){
-        elementActions.click(ClickTo2BTN).click(filterBTN).click(tipPlateja).click(convertaciyaTip).click(primenitBTN);
+    public void PoNaimenovaniu(){
+        elementActions.click(filterBTN).sendKeys(naimenovanieField,"ОсОО \"АЛТЫНКЕН\"").click(primenitBTN);
     }
+    public void PoNumberDocument(){
+        elementActions.click(filterBTN).sendKeys(numberDocumentField,"Test value").click(primenitBTN);
+    }
+    public void PoValute(){
+        elementActions.click(filterBTN).sendKeys(volutaField,"KGS").click(KGSChoose).click(primenitBTN);
+    }
+    public void Data(){
+        elementActions.sendKeys(dataOT,"06072023").sendKeys(dataDO,"07072023").click(primenitBTN);
+    }
+    public void Summa(){
+        elementActions.sendKeys(summaOt,"1000").sendKeys(summaDO,"10000").click(primenitBTN);
+    }
+
+
+
+
+    public void TipPlatejaFilterConverting(){
+        elementActions.click(filterBTN).click(tipPlateja).click(convertaciyaTip).click(primenitBTN);
+    }
+    public void TipPlatejaFilterGross(){
+        elementActions.click(filterBTN).click(tipPlateja).click(grossTip).click(primenitBTN);
+    }
+    public void TipPlatejaFilterClearing(){
+        elementActions.click(filterBTN).click(tipPlateja).click(clearingTip).click(primenitBTN);
+    }
+    public void TipPlatejaFilterSWIFT(){
+        elementActions.click(filterBTN).click(tipPlateja).click(swiftTip).click(primenitBTN);
+    }
+    public void TipPlatejaFilterPErevodNADrugie(){
+        elementActions.click(filterBTN).click(tipPlateja).click(perevodNADrugieClientskieTip).click(primenitBTN);
+    }
+    public void TipPlatejaFilterPerevodMejdu(){
+        elementActions.click(filterBTN).click(tipPlateja).click(perevodMejduSvoimiTip).click(primenitBTN);
+    }
+
+
+
+
+
 
     public OperationHistory BTN2(){
         elementActions.click(ClickTo2BTN);
@@ -92,10 +136,6 @@ public class OperationHistory extends BasePage {
 //    }
     public OperationHistory NaPodpis(){
         elementActions.click(naPodpis);
-        return this;
-    }
-    public OperationHistory OjidaetOtpravki(){
-        elementActions.click(ojidaetOtpravki);
         return this;
     }
     public OperationHistory NaRedactirov(){
@@ -115,7 +155,7 @@ public class OperationHistory extends BasePage {
         return this;
     }
     public OperationHistory MoiChernoviki(){
-        elementActions.click(moiChernoviki);
+        elementActions.click(moiChernoviki).click(allBTN);
         return this;
     }
 
